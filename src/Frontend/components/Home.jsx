f793efe0ba77067c3c26bef32c81d522da1b034a
jesidmsanz69 <jesidmoscote@gmail.com>
2020-11-26 02:00:35 -0500

    update

--- src/Frontend/components/Home.jsx
+++ src/Frontend/components/Home.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
+import Nabvar from "./Nabvar";
 
 export default class Homepage extends Component {
   render() {
     return (
       <div>
+        <Nabvar />
         <h1>Hola</h1>
-        <strong>Doctor</strong>
+        <h1>Doctor</h1>
       </div>
     );
   }
---
+++ src/Frontend/components/Nabvar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Nabvar() {
+  return (
+    <div className="navbar-items">
+      <div className="navbar-logo">Maranatha en Gloria</div>
+
+      <div className="menu-icon"></div>
+
+      <div className="nav-links">
+        <ul>
+          <li>Sobre Nosotros</li>
+          <li>Ministerios</li>
+          <li>Escuelas Ministeriales</li>
+          <li>Donaciones</li>
+          <li>Contactanos</li>
+          <li>Señal en vivo</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Nabvar;
