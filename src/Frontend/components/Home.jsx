cc2ed955b204f0cfb70d993599b1e50b5d726044
jesidmsanz69 <jesidmoscote@gmail.com>
2020-11-26 01:54:19 -0500

    Jesidmsanz

---
+++ src/Frontend/components/Home.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Homepage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola</h1>
+        <strong>Doctor</strong>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Homepage from './Frontend/components/Home'
+
+ReactDOM.render(<Homepage />, document.getElementById('root'));
+
